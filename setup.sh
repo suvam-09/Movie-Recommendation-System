e9d038425e6d2a049f70b37960a5283c28a0e474
Suvam Kumar <suvam.kumar@yahoo.com>
2022-11-04 00:00:38 +0530

    commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
+# creating a directory on the server
 mkdir -p ~/.streamlit/
 
+# establishing connection with the server
 echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml # creating a configuration file
