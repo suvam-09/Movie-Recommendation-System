300b6b8ee1bb14a9e454d0a8f88488e323fc0dc7
Suvam Kumar <suvam.kumar@yahoo.com>
2022-11-04 12:42:50 +0530

    setup file

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+from setuptools import find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-# edit below variables as per the requirements -
-REPO_NAME = "Movie-Recommendation-System"
-AUTHOR_USER_NAME = "suvam-09"
-SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit', 'requests', 'pickleshare']
-
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
 
 setup(
-    name=SRC_REPO,
-    version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    description="Movie Recommender System",
+    # Name of the package
+    name='Movie-Recommendation-System',
+    # Packages to include into the distribution
+    packages=find_packages('.'),
+    # Start with a small number and increase it with
+    # every change you make https://semver.org
+    version='1.0.0',
+    # Chose a license from here: https: //
+    # help.github.com / articles / licensing - a -
+    # repository. For example: MIT
+    license='',
+    # Short description of your library
+    description='TMDB Movie Recommendation SYstem using Machine Learning',
+    # Long description of your library
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="suvam.kumar@yahoo.com",
-    packages=[SRC_REPO],
-    python_requires=">=3.7",
-    install_requires=LIST_OF_REQUIREMENTS
+    long_description_content_type='Designing a streamlit web application that can recommend various kinds of similar movies based on users\' interest. Recommendation systems work in a way wherein the users are requested to type/select a movie based on their interest and the recommender system would help suggest back a list of movies similar to the one selected by the user.',
+    # Your name
+    author='Suvam Kumar',
+    # Your email
+    author_email='suvam.kumar@yahoo.com',
+    # Either the link to your github or to your website
+    url='https://blink-movie-recommender.herokuapp.com/',
+    # Link from which the project can be downloaded
+        download_url='https://github.com/suvam-09/Movie-Recommendation-System',
+        # List of keywords
+        keywords=[],
+        # List of packages to install with this one
+        install_requires=['streamlit', 'requests', 'pickleshare'],
+        # https://pypi.org/classifiers/
+        classifiers=[]
 )
